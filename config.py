632cfe3f0aa6bea342668717f8ca693b1681c50b
XinyaoTian <leontian1024@gmail.com>
2019-09-18 17:59:30 +0800

    K8s config

--- config.py
+++ config.py
@@ -14,13 +14,13 @@ class Config(object):
 
     # ------------------------------ #
     # # K8s config
-    # POSTGRES_HOST = os.environ.get('POSTGRES_SERVICE_HOST')
-    # POSTGRES_PORT = os.environ.get('POSTGRES_SERVICE_PORT')
-    # POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME')
-    # POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
-    # POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    # POSTGRES_PROTOCOL = os.environ.get('POSTGRES_PROTOCOL')
-    # POSTGRES_SSLMODE = os.environ.get('POSTGRES_SSLMODE')
+    POSTGRES_HOST = os.environ.get('POSTGRES_SERVICE_HOST')
+    POSTGRES_PORT = os.environ.get('POSTGRES_SERVICE_PORT')
+    POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME')
+    POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    POSTGRES_PROTOCOL = os.environ.get('POSTGRES_PROTOCOL')
+    POSTGRES_SSLMODE = os.environ.get('POSTGRES_SSLMODE')
     #
     # # assembled Database URI
     # SQLALCHEMY_DATABASE_URI = POSTGRES_PROTOCOL + '://' + POSTGRES_USERNAME + ':' + POSTGRES_PASSWORD + '@' +\
@@ -30,7 +30,7 @@ class Config(object):
     # protocol + `://` + dbUsername + `:` + dbPassword + `@` + dbIp + `:` + dbPort + `/` + dbName + `?sslmode=` +sslMode
     # config example:
     # SQLALCHEMY_DATABASE_URI = 'postgres://dbuser:docker@174.137.53.253:5432/testdb?sslmode=disable'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
