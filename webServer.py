f534e104f05392bf7548a5259464a9f3b66ccb34
XinyaoTian <leontian1024@gmail.com>
2019-09-18 22:16:24 +0800

    update config

--- webServer.py
+++ webServer.py
@@ -1,11 +1,16 @@
 from app import app
+import time
+from config import Config
 import os
 
 import logging
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    os.system('flask db init')
-    os.system("flask db migrate -m'Server running on K8s.'")
-    os.system('flask db upgrade')
+    # os.system('flask db init')
+    # os.system("flask db migrate -m'Server running on K8s.'")
+    # os.system('flask db upgrade')
+    while Config.POSTGRES_HOST is None or Config.POSTGRES_PORT is None:
+        logging.error('Waiting for parameter POSTGRES_HOST and POSTGRES_PORT...')
+        time.sleep(3)
     app.run(host='0.0.0.0', port=5000, debug=False)
